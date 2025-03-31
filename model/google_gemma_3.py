65a602cb9b435c9cd82fb68edde356e885d1813c
Hyun-je <bigae2@gmail.com>
2025-03-31 17:14:17 +0900

    새로운 이미지 처리 기능을 추가하고, 텍스트와 이미지 쌍을 인코딩하는 데이터 콜레이터를 구현했습니다. 이를 통해 메시지에서 이미지를 추출하고, 손실 계산을 위한 레이블 마스킹을 개선했습니다.

---
+++ model/google_gemma_3.py
@@ -0,0 +1,56 @@
+from PIL import Image
+
+
+def process_vision_info(messages: list[dict]) -> list[Image.Image]:
+    image_inputs = []
+    # Iterate through each conversation
+    for msg in messages:
+        # Get content (ensure it's a list)
+        content = msg.get("content", [])
+        if not isinstance(content, list):
+            content = [content]
+
+        # Check each content element for images
+        for element in content:
+            if isinstance(element, dict) and (
+                "image" in element or element.get("type") == "image"
+            ):
+                # Get the image and convert to RGB
+                if "image" in element:
+                    image = element["image"]
+                else:
+                    image = element
+                image_inputs.append(image.convert("RGB"))
+    return image_inputs
+
+
+def collate_fn(examples, processor):
+    texts = []
+    images = []
+    for example in examples:
+        image_inputs = process_vision_info(example["message"])
+        text = processor.apply_chat_template(
+            example["message"], add_generation_prompt=False, tokenize=False
+        )
+        texts.append(text.strip())
+        images.append(image_inputs)
+
+    # Tokenize the texts and process the images
+    batch = processor(text=texts, images=images, return_tensors="pt", padding=True)
+
+    # The labels are the input_ids, and we mask the padding tokens and image tokens in the loss computation
+    labels = batch["input_ids"].clone()
+
+    # Mask image tokens
+    image_token_id = [
+        processor.tokenizer.convert_tokens_to_ids(
+            processor.tokenizer.special_tokens_map["boi_token"]
+        )
+    ]
+    # Mask tokens for not being used in the loss computation
+    labels[labels == processor.tokenizer.pad_token_id] = -100
+    labels[labels == image_token_id] = -100
+    labels[labels == 262144] = -100
+
+    batch["labels"] = labels
+    return batch
